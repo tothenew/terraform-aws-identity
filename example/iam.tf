7568fb69d2e46f3f5e8098d47cb5dfe663d1a78e
sparsh-anurag <sparsh.anurag@tothenew.com>
2023-02-20 16:07:18 +0530

    Updated Assume Role Policy

--- example/iam.tf
+++ example/iam.tf
@@ -1,11 +1,7 @@
-# module "iam" {
-#     source     = "git::https://github.com/tothenew/terraform-aws-identity.git"
-#     saml_provider_name="TestUser1"
-# }
-
 module "iam" {
-    source     = "../"
+    source     = "git::https://github.com/tothenew/terraform-aws-identity.git"
     saml_provider_name="TestUser1"
 }
+
 
 
