683473954a051e1615a3d9d734f0833aaa098698
Saurabh0703 <66934981+Saurabh0703@users.noreply.github.com>
2023-05-11 09:59:39 +0530

    Update iam_terraform_tf_agent.tf

--- iam_terraform_tf_agent.tf
+++ iam_terraform_tf_agent.tf
@@ -61,23 +61,19 @@ resource "aws_iam_policy" "additional_policy" {
 EOF
 }
 resource "aws_iam_role_policy_attachment" "additional_policy_attachment" {
-  count = var.create_iam_terraform_tf_agent_role ? 1 : 0
-  role       = aws_iam_role.terraform_tf_agent[0].name
+  role       = aws_iam_role.terraform_tf_agent.name
   policy_arn = aws_iam_policy.additional_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "vpc" {
-  count = var.create_iam_terraform_tf_agent_role ? 1 : 0
-  role       = aws_iam_role.terraform_tf_agent[0].name
+  role       = aws_iam_role.terraform_tf_agent.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
 resource "aws_iam_role_policy_attachment" "ssmFull" {
-  count = var.create_iam_terraform_tf_agent_role ? 1 : 0
-  role       = aws_iam_role.terraform_tf_agent[0].name
+  role       = aws_iam_role.terraform_tf_agent.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 resource "aws_iam_role_policy_attachment" "autoscaling" {
-  count = var.create_iam_terraform_tf_agent_role ? 1 : 0
-  role       = aws_iam_role.terraform_tf_agent[0].name
+  role       = aws_iam_role.terraform_tf_agent.name
   policy_arn = "arn:aws:iam::aws:policy/AutoScalingFullAccess"
 }
